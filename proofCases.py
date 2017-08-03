8113c6011f8fea3f90a59dab4dc5935785a56fc7
kcf-jackson <kcf.jackson@gmail.com>
2017-08-03 14:33:34 +1000

    Add test cases and back up

---
+++ proofCases.py
@@ -0,0 +1,17 @@
+#==============================================================================
+# Main
+#==============================================================================
+exec(open('setup.py').read())
+
+# Proof of Markov's inequality
+print("Markov's inequality")
+lhs = parseTree("( a * I ( X >= a ) )")
+op = "<="
+rhs = parseTree("X")
+masterEq = Equation(lhs, op, rhs)
+masterEq.view()
+
+masterEq.applyToBoth(div_X, Node('a', 'symbol'))
+masterEq.applyToLeft(simplifyDiv)
+masterEq.applyToBoth(takeExpectation)
+masterEq.applyToLeft(functionalToMeasure)
