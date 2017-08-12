99cf5bea6ecb0d25fecfe159abb35cfe638196bb
kcf-jackson <kcf.jackson@gmail.com>
2017-08-12 19:35:14 +1000

    Perform minor edit

--- proof_machine/manipulation/operatorRelation.py
+++ proof_machine/manipulation/operatorRelation.py
@@ -5,10 +5,17 @@ def invariant(tree):
 			return tree.getLeftChild()
 	return tree
 
-def symmetric(tree):
+def symmetry(tree):
 	op = tree.getRootNodeValue()
-	if op in ['cap', 'cup', '*', '+', ]:
+	if op in ['cap', 'cup', '*', '+']:
 		resTree = BinaryTree(tree.getRootNode())
 		resTree.leftChild, resTree.rightChild = tree.rightChild, tree.leftChild
 		return resTree
+	return tree
+
+def tautology(tree, op1, op2, sym):
+	# example: tautology(tree, '+', '-', 'a') returns tree + a - a
+	symNode = Node(sym, 'symbol')
+	tree = addTopRight(tree, Node(op1, 'operator'), symNode)
+	tree = addTopRight(tree, Node(op2, 'operator'), symNode)
 	return tree
