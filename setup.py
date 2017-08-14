9ce022bdc2e57d5da3e424fbee47bf7721e8d32e
kcf-jackson <kcf.jackson@gmail.com>
2017-08-14 11:26:02 +1000

    Update version number

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='proof_machine',
-      version='0.2',
+      version='0.27',
       description='A machine to do proof in statistics and probability',
       url='http://github.com/kcf-jackson/Proof_machine',
       author='Jackson Kwok',
       author_email='kcf.jackson@gmail.com',
       license='MIT',
       packages=['proof_machine', 'proof_machine.objClass', 'proof_machine.manipulation', 'proof_machine.factory'],
-      install_requires=['pythonds'],
+      install_requires=['pythonds', 'orderedset'],
       zip_safe=False)
