ecaa85bbc0e8eb94b6fbf99336d3296a83330045
kcf-jackson <kcf.jackson@gmail.com>
2017-09-29 19:15:21 +1000

    Check files in the others folder with pylint

--- proof_machine/others/initialise_variables.py
+++ proof_machine/others/initialise_variables.py
@@ -1,3 +1,5 @@
+"""This file contains functions to initialise a default namespace containing the commonly used
+variables and the operator precedence table."""
 import string
 
 
@@ -7,32 +9,35 @@ def default_namespace():
     Attributes:
         Symbols: alphaNumeric, greekAlphabet
 
-        Functions: trigonometricFunction, hyperbolicFunction, elementaryFunction, setFunction, integrationFunction,
-        probabilityFunction, arithmeticFunction, vectorSpaceFunction
+        Functions: trigonometricFunction, hyperbolicFunction, elementaryFunction, setFunction,
+        integrationFunction, probabilityFunction, arithmeticFunction, vectorSpaceFunction
 
         Operators: arithmeticOperator, logicOperator, setOperator, otherOperator
 
         Parentheses: parentheses
     """
     return dict(alphaNumeric=list(string.digits) + list(string.ascii_lowercase) + list(string.ascii_uppercase),
-                greekAlphabet=['alpha', 'beta', 'chi', 'delta', 'digamma', 'epsilon', 'eta', 'gamma', 'iota', 'kappa',
-                               'lambda', 'mu', 'nu', 'omega',
-                               'phi', 'pi', 'psi', 'rho', 'sigma', 'tau', 'theta', 'upsilon', 'varepsilon', 'varkappa',
-                               'varphi', 'varpi', 'varrho', 'varsigma',
-                               'vartheta', 'xi', 'zeta', 'Delta', 'Gamma', 'Lambda', 'Omega', 'Phi', 'Pi', 'Psi',
-                               'Sigma',
-                               'Theta', 'Upsilon', 'Xi', 'mho', 'nabla'],
-                trigonometricFunction=['sin', 'cos', 'tan', 'sec', 'csc', 'cot', 'arcsin', 'arccos', 'arctan', 'arcsec',
-                                       'arccsc', 'arccot'],
-                hyperbolicFunction=['sinh', 'cosh', 'tanh', 'sech', 'csch', 'coth', 'arsinh', 'arcosh', 'artanh',
-                                    'arsech',
-                                    'arcsch', 'arcoth'], elementaryFunction=['exp', 'log', 'abs', 'sqrt', 'max', 'min'],
+                greekAlphabet=['alpha', 'beta', 'chi', 'delta', 'digamma', 'epsilon', 'eta',
+                               'gamma', 'iota', 'kappa', 'lambda', 'mu', 'nu', 'omega', 'phi',
+                               'pi', 'psi', 'rho', 'sigma', 'tau', 'theta', 'upsilon',
+                               'varepsilon', 'varkappa', 'varphi', 'varpi', 'varrho', 'varsigma',
+                               'vartheta', 'xi', 'zeta', 'Delta', 'Gamma', 'Lambda', 'Omega', 'Phi',
+                               'Pi', 'Psi', 'Sigma', 'Theta', 'Upsilon', 'Xi', 'mho', 'nabla'],
+                trigonometricFunction=['sin', 'cos', 'tan', 'sec', 'csc', 'cot', 'arcsin', 'arccos',
+                                       'arctan', 'arcsec', 'arccsc', 'arccot'],
+                hyperbolicFunction=['sinh', 'cosh', 'tanh', 'sech', 'csch', 'coth', 'arsinh',
+                                    'arcosh', 'artanh', 'arsech', 'arcsch', 'arcoth'],
+                elementaryFunction=['exp', 'log', 'abs', 'sqrt', 'max', 'min'],
                 setFunction=['I', 'sup', 'inf', 'limsup', 'liminf', 'cap_i', 'cup_i', 'cplm'],
-                integrationFunction=['int', 'D'], probabilityFunction=['E', 'P'],
-                arithmeticFunction=['sum_i', 'prod_i'], vectorSpaceFunction=['norm'],
+                integrationFunction=['int', 'D'],
+                probabilityFunction=['E', 'P'],
+                arithmeticFunction=['sum_i', 'prod_i'],
+                vectorSpaceFunction=['norm'],
                 arithmeticOperator=['+', '-', '*', '/', '^', '<', '>', '<=', '>=', '='],
-                logicOperator=['=>', '<=>', 'land', 'lor'], setOperator=['cup', 'cap', 'setDiff', 'symSetDiff'],
-                otherOperator=['|', ','], parentheses=['{', '[', '(', ')', ']', '}'])
+                logicOperator=['=>', '<=>', 'land', 'lor'],
+                setOperator=['cup', 'cap', 'setDiff', 'symSetDiff'],
+                otherOperator=['|', ','],
+                parentheses=['{', '[', '(', ')', ']', '}'])
 
 
 def get_precedence_table():
